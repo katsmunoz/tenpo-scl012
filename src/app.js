4539267a13251df779b1ce741e938b7617e34538
kaarenzu <karenzcortes0211@gmail.com>
2020-05-18 16:00:05 -0400

    Agregué cambios en la vista de loginEmail

--- src/app.js
+++ src/app.js
@@ -3,3 +3,16 @@
 //     beforeLogin.style.display = 'none';
 //     emailLogin.style.display = 'block';
 //   });
+
+// function Registrar () {
+ 
+//     const email = document.getElementById("email");
+
+// email.addEventListener("input", function (event) {
+//   if (email.validity.typeMismatch) {
+//     email.setCustomValidity("I am expecting an e-mail address!");
+//   } else {
+//     email.setCustomValidity("");
+//   }
+// });
+// }
